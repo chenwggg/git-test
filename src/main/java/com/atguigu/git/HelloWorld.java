bdfbe60c30e0b26aff5520452d93ed16cd4acbf1
chenwenguang666 <chenwenguang666@163.com>
2023-02-04 21:09:17 +0800

    commit 1

---
+++ src/main/java/com/atguigu/git/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * TODO git测试
+ * Created by chenwenguang on 2023/2/4
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
